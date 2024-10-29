b87973396b06b8ab79bdf25f8fd96156e144799a
LOWZIMIN <lowzm.23@ichat.sp.edu.sg>
2024-10-29 11:57:16 +0800

    lab3

---
+++ Test_bmi.py
@@ -0,0 +1,13 @@
+from Lab2.bmi import calculate_bmi
+
+def test_bmi_under_weight():
+    # Test for BMI value that should be classified as "Under Weight"
+    assert calculate_bmi(1.8, 50) == -1
+
+def test_bmi_normal_weight():
+    # Test for BMI value that should be classified as "Normal Weight"
+    assert calculate_bmi(1.75, 70) == 0
+
+def test_bmi_over_weight():
+    # Test for BMI value that should be classified as "Over Weight"
+    assert calculate_bmi(1.6, 80) == 1
